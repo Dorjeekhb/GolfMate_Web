01a369a5ac84a6a8a3b5b649e8627c02e23df6ea
anonymous <99793450+sosanc03@users.noreply.github.com>
2025-03-05 17:02:27 +0100

    Update animations.js

--- scripts/animations.js
+++ scripts/animations.js
@@ -22,16 +22,37 @@
     const golfBall = document.getElementById("golfBall");
     let posX = 0;
     const speed = 2; // píxeles por frame
-  
+    let stop = true;
+
+    window.onscroll = function() {
+      // Calculamos la posición actual del scroll y la altura total de la página
+      const scrollPosition = window.scrollY;
+      const documentHeight = document.documentElement.scrollHeight;
+      const windowHeight = window.innerHeight;
+    
+      // Si la posición del scroll más la altura de la ventana es igual o mayor a la altura total del documento
+      if (scrollPosition + windowHeight >= 2556) {
+        stop = false;
+        posX = -40;
+      }
+      else {
+        posX = -100;
+        stop = true;
+      }
+      requestAnimationFrame(animateBall);
+    };
+    
     function animateBall() {
       posX += speed;
       if (posX > window.innerWidth) {
         posX = -40; // Reinicia la posición cuando sale de la pantalla
       }
       golfBall.style.left = posX + "px";
-      requestAnimationFrame(animateBall);
+      if(!stop)requestAnimationFrame(animateBall);
     }
     if (golfBall) {
+      stop = true;
+      posX = -100;
       animateBall();
     }
   });
